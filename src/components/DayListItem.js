8d99bf7af6daffb5b3e9ad8a2219730eff4a6fbd
Aaron <timbolaj@gmail.com>
2020-06-08 15:24:12 -0400

    Finished Listing Days III

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,9 +2,22 @@ import React from "react";
 import "components/DayListItem.scss";
 const classNames = require('classnames');
 
+const formatSpots = spots => {
+  if (!spots) {
+    return `no spots remaining`;
+  }
+
+  if (spots === 1) {
+    return `${spots} spot remaining`;
+  }
+
+  return `${spots} spots remaining`;
+}
+
 //Props are name, selected, spots and setday
 
 export default function DayListItem(props) {
+  const availabilityMessage = formatSpots(props.spots);
 
   let dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
@@ -17,7 +30,7 @@ export default function DayListItem(props) {
       onClick={() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{availabilityMessage}</h3>
     </li>
   );
 }
