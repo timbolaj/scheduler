45d15eb5546f4fdf68023ae87c6e18642bf2e5d1
Aaron <timbolaj@gmail.com>
2020-06-08 18:26:47 -0400

    Added InterviewerListItem

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import 'components/InterviewerListItem.scss'
+const classNames = require('classnames');
+
+/* Takes and id, name, avatar (url), selected, setInterviewer function(sets interviewer)
+*/
+
+export default function InterviewerListItem(props) {
+  let interviewerStyles = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+    "interviewers__item": !props.selected
+  })
+
+  let imageStyles = classNames("interviewers__item-image", {
+    "interviewers__item--selected-image": props.selected,
+    "interviewers__item-image": !props.selected
+  })
+
+  return (
+    <li 
+      className={interviewerStyles}
+      onClick={() => props.setInterviewer(props.name)}
+    >
+      <img
+        className={imageStyles}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.name}
+    </li>
+  )
+}
+
