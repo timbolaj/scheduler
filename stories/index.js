ae9b6c4e6315239e8b3493d6cbf964df1c7de20c
Aaron <timbolaj@gmail.com>
2020-06-09 12:43:55 -0400

    Finished Listing Interviewers II

--- stories/index.js
+++ stories/index.js
@@ -97,7 +97,7 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
         id={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        setInterviewer={action("setInterviewer")}
+        setInterviewer={event => action("setInterviewer")(interviewer.id)}
       />
     ));
 
@@ -122,7 +122,7 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
     .add("Preselected", () => (
       <InterviewerList
         interviewers={interviewers}
-        interviewer={3}
-        setInterviewer={action("setInterviewer")}
+        value = {3}
+        onChange={action("onChange")}
       />
     ));
