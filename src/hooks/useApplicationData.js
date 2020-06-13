0737ac103bf6b65d51213f5741636a134bd612ba
Aaron <timbolaj@gmail.com>
2020-06-13 15:07:36 -0400

    Am now tracking the number of spots left in a given day in realtime

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -22,9 +22,20 @@ export default function useApplicationData() {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     })
   }, [])
-
+  
+  function findDay(day) {
+    const daysOfWeek = {
+      Monday: 0,
+      Tuesday: 1,
+      Wednesday: 2,
+      Thursday: 3,
+      Friday: 4
+    }
+    return daysOfWeek[day]
+  }
+  
   function bookInterview(id, interview) {
-
+ 
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
@@ -35,11 +46,21 @@ export default function useApplicationData() {
       [id]: appointment
     };
 
+    const dayOfWeek = findDay(state.day)
+
+    const day = {
+      ...state.days[dayOfWeek],
+      spots: state.days[dayOfWeek].spots - 1
+    }
+
+    let days = state.days
+    days[dayOfWeek] = day;
+
     return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview:interview})
     .then(res => {
-        setState({...state, appointments})
+        setState({...state, appointments, days})
         return res
-      })
+    })
   }
 
   function cancelInterview(id) {
@@ -53,10 +74,20 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     }
+
+    const dayOfWeek = findDay(state.day)
+
+    const day = {
+      ...state.days[dayOfWeek],
+      spots: state.days[dayOfWeek].spots + 1
+    }
+
+    let days = state.days
+    days[dayOfWeek] = day;
     
     return axios.delete(`http://localhost:8001/api/appointments/${id}`)
     .then(res => {
-      setState({...state, appointments})
+      setState({...state, appointments, days})
       return res
     })
   }
